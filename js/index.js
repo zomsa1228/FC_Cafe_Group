8d6c516c447c4ac110288af4b75ea528cd2c17ce
zomsa1228 <Adventure1611@gmail.com>
2019-10-15 09:01:22 +0900

    ホームページを総入れ替え

--- js/index.js
+++
@@ -1,6 +0,0 @@
-$('document').ready(function() {
-	var options = { videoId: 'MYG8kyImHPY', start: 3 };
-	$('#wrapper').tubular(options);
-	// f-UGhWj1xww cool sepia hd
-	// 49SKbS7Xwf4 beautiful barn sepia
-});
